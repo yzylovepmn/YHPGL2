8704702c9f3dd6877c60f3be4cf2bea03fca9275
哲宇 杨 <7836596@qq.com>
2020-11-19 14:40:30 +0800

    添加项目文件。

---
+++ YHPGL2/HPGLFile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YHPGL2
+{
+    public class HPGLFile
+    {
+    }
+}
