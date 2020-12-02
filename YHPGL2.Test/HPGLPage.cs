865cf0686fb543b94c180d0ce802ead86a1efaf2
哲宇 杨 <7836596@qq.com>
2020-12-02 09:49:36 +0800

    test code

---
+++ YHPGL2.Test/HPGLPage.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+
+namespace YHPGL2.Test
+{
+    public class HPGLPage : UIElement
+    {
+        private static System.Windows.Media.Pen _pen;
+
+        static HPGLPage()
+        {
+            _pen = new System.Windows.Media.Pen(Brushes.Black, 1);
+        }
+
+        public HPGLPage()
+        {
+            _transform = new MatrixTransform();
+        }
+
+        public IEnumerable<IShape> Shapes 
+        { 
+            get { return _shapes; } 
+            set
+            { 
+                _shapes = value.ToList();
+                InvalidateVisual();
+            }
+        }
+        private List<IShape> _shapes;
+
+        private MatrixTransform _transform;
+        private Matrix _matrix;
+
+        public void Zoom(double scaleX, double scaleY)
+        {
+            Zoom(scaleX, scaleY, 0, 0);
+        }
+
+        public void Zoom(double scaleX, double scaleY, double centerX, double centerY)
+        {
+            _matrix.ScaleAt(scaleX, scaleY, centerX, centerY);
+            _transform.Matrix = _matrix;
+            _pen.Thickness = 1 / _matrix.M11;
+            InvalidateVisual();
+        }
+
+        protected override void OnRender(DrawingContext drawingContext)
+        {
+            base.OnRender(drawingContext);
+            if (_shapes != null)
+            {
+                drawingContext.PushTransform(_transform);
+                var geo = new StreamGeometry();
+                using (var stream = geo.Open())
+                {
+                    foreach (var shape in _shapes)
+                        _DrawShape(stream, shape);
+                }
+                drawingContext.DrawGeometry(null, _pen, geo);
+            }
+        }
+
+        private void _DrawShape(StreamGeometryContext stream, IShape shape)
+        {
+            switch (shape.Type)
+            {
+                case ShapeType.PolyLine:
+                    {
+                        var polyLine = (PolyLine)shape;
+                        var points = polyLine.Points.Select(p => new Point(p.X / 40, RenderSize.Height - p.Y / 40));
+                        stream.BeginFigure(points.First(), false, polyLine.IsClosed);
+                        stream.PolyLineTo(points.Skip(1).ToList(), true, true);
+                    }
+                    break;
+                case ShapeType.Polygon:
+                    break;
+                case ShapeType.Complex:
+                    break;
+            }
+        }
+    }
+}
--- YHPGL2.Test/MainWindow.xaml.cs
+++ YHPGL2.Test/MainWindow.xaml.cs
@@ -24,9 +24,12 @@ namespace YHPGL2.Test
         public MainWindow()
         {
             InitializeComponent();
+            _page = new HPGLPage();
+            GD_Page.Children.Add(_page);
         }
 
         private HPGLFile _hpglFile;
+        private HPGLPage _page;
 
         private void _OnClicked(object sender, RoutedEventArgs e)
         {
@@ -37,7 +40,18 @@ namespace YHPGL2.Test
         {
             var dialog = new OpenFileDialog() { Filter = string.Format("{0}|*.{1};*.{2};*.{3};", "HPGL File", "plt", "hgl", "hpgl") };
             if (dialog.ShowDialog() == true)
+            {
                 _hpglFile = HPGLFile.Load(dialog.FileName);
+                _page.Shapes = _hpglFile.Execute(GD_Page.RenderSize);
+            }
+        }
+
+        protected override void OnMouseWheel(MouseWheelEventArgs e)
+        {
+            base.OnMouseWheel(e);
+            var p = e.GetPosition(_page);
+            var scale = 1 + e.Delta / 1000.0;
+            _page.Zoom(scale, scale, p.X, p.Y);
         }
     }
 }
